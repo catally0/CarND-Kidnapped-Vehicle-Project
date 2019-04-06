6d1ae303b3d5997ce7e944c81fc9434343194fd4
MarshallMa21 <35001965+MarshallMa21@users.noreply.github.com>
2019-04-06 14:12:51 +0800

    add init function (with debug print)

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -21,6 +21,9 @@
 using std::string;
 using std::vector;
 
+using std::normal_distribution;
+using std::default_random_engine;
+
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
   /**
    * TODO: Set the number of particles. Initialize all particles to 
@@ -30,7 +33,29 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 0;  // TODO: Set the number of particles
+  std::default_random_engine gen;
+
+  normal_distribution<double> dist_x(x, std[0]);
+  normal_distribution<double> dist_y(y, std[1]);
+  normal_distribution<double> dist_theta(theta, std[2]);
+
+  num_particles = 1000;  // TODO: Set the number of particles
+
+  for (int i = 0; i < num_particles; ++i) {
+    Particle p;
+    
+    p.id = i;
+    p.x = dist_x(gen);
+    p.y = dist_y(gen);
+    p.theta = dist_theta(gen); 
+    p.weight = 1;
+    
+    particles.push_back(p);
+
+    // Print your samples to the terminal.
+    std::cout << "Init #" << i << ": " << p.x << " " << p.y << " " 
+              << p.theta << std::endl;
+  }
 
 }
 
